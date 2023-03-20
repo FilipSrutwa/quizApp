19da92d58dfe63abdaeb22a308caee9bda4c2935
FilipSrutwa <karok344@gmail.com>
2023-03-20 13:16:45 +0100

    Initial commit

---
+++ app/Views/mainMenu.php
@@ -0,0 +1,9 @@
+<?= $this->extend('layouts/topBottom') ?>
+<?= $this->section('content') ?>
+
+<div class="container-fluid">
+    <h1 style="text-align:center;">Menu główne</h1>
+
+    </form>
+</div>
+<?= $this->endSection() ?>
